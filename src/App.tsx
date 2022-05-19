b374d88618a04cefe8a0ee966447ed001c133160
Gabriel Thomé <gabrielallan.thome@gmail.com>
2022-05-18 23:49:48 -0300

    home redirects to login if user is not authorized

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { Login } from './Login';
+import { MyLists } from './MyLists';
 
 export function App() {
-    return <Login />;
+    const authorizationExpireDate = localStorage.getItem('expire_date');
+    if (!authorizationExpireDate || new Date(authorizationExpireDate) < new Date()) {
+        return <Login />;
+    }
+
+    return <MyLists />;
 }
