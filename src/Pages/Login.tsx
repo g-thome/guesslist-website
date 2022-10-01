8c31081dd424db8585a938b40a592ef74fd3f1da
Gabriel Thomé <gabrielallan.thome@gmail.com>
2022-10-01 15:26:36 -0300

    use next/link instead of manually modifying the url

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,11 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import DiscordLogo from "../../public/discord-logo.svg";
 import { DISCORD_AUTHORIZATION_URL } from "../constants";
-
-function goToAuth() {
-  window.location.href = DISCORD_AUTHORIZATION_URL;
-}
 
 export default function Login() {
   return (
@@ -13,12 +10,11 @@ export default function Login() {
       <div className="mb-2">
         <Image src={DiscordLogo} alt="discord" width={200} height={200} />
       </div>
-      <button
-        className="cursor-pointer bg-veryLightBlue outline-none border-0 py-2 px-4 text-white w-fit"
-        onClick={goToAuth}
-      >
-        LOGIN WITH DISCORD
-      </button>
+      <Link href={DISCORD_AUTHORIZATION_URL}>
+        <button className="cursor-pointer bg-veryLightBlue outline-none border-0 py-2 px-4 text-white w-fit">
+          LOGIN WITH DISCORD
+        </button>
+      </Link>
     </div>
   );
 }
