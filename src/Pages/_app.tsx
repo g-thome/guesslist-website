30094bbf1df64a315e8730c398cb277649ee204a
Gabriel Thomé <gabrielallan.thome@gmail.com>
2022-10-12 03:41:53 -0300

    make index.css have only the necessary tailwind calls

--- src/Pages/_app.tsx
+++ src/Pages/_app.tsx
@@ -1,16 +1,24 @@
 import { SessionProvider } from "next-auth/react";
 import "../index.css";
 import { GlobalState } from "../context/GlobalState";
+import Head from "next/head";
 
 export default function MyApp({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <GlobalState>
-        <Component {...pageProps} />
-      </GlobalState>
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Guess List</title>
+      </Head>
+      <SessionProvider session={session}>
+        <GlobalState>
+          <div className="bg-onyx">
+            <Component {...pageProps} />
+          </div>
+        </GlobalState>
+      </SessionProvider>
+    </>
   );
 }
