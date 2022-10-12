2b79b7bd7475184bd4c1f511f27e89e3c67909af
Gabriel Thomé <gabrielallan.thome@gmail.com>
2022-10-12 03:03:38 -0300

    removed unused functions

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,6 @@ export const DISCORD_API_USER_INFO = DISCORD_API_URL + "/users/@me";
 export const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
 export const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
 export const DISCORD_AUTHORIZATION_URL = process.env.DISCORD_AUTHORIZATION_URL;
-export const DISCORD_CDN = "https://cdn.discordapp.com";
 export const API_URL = process.env.API_URL;
 export const ADMIN_ID = process.env.ADMIN_ID;
 export const BOT_AVATAR =
--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,1 @@
-import { DISCORD_CDN } from "./constants";
-
-export const fetcher = (url: string) => fetch(url).then(res => res.json())
-
-export const fetcherWithToken = (url: string, token) => {
-    return fetch(url, {
-        method: 'GET',
-        headers: {
-            Authorization: 'Bearer ' + token
-        }
-    }).then((res) => res.json());
-};
-
-// https://discord.com/developers/docs/reference#image-formatting
-export const discordImage = (avatarHash: string) => `${DISCORD_CDN}/${avatarHash}`
+export const fetcher = (url: string) => fetch(url).then((res) => res.json());
