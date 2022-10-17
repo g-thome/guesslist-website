9fec864817dc05ea876780b254e6361eebe5eaca
Gabriel Thomé <gabrielallan.thome@gmail.com>
2022-10-17 11:51:59 -0300

    ci: run dev server for playwright

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("Login page has Guess List as document title", async ({ page }) => {
-  await page.goto("http://localhost:3000/login");
+  await page.goto("/");
 
   await expect(page).toHaveTitle("Guess List");
 });
