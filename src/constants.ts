bbfebe041625cc940f80403de8c34af61adb4ca6
Gabriel Thomé <gabrielallan.thome@gmail.com>
2022-10-17 10:38:39 -0300

    fix: API utility class not reaching api url

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const DISCORD_API_USER_INFO = DISCORD_API_URL + "/users/@me";
 export const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
 export const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
 export const DISCORD_AUTHORIZATION_URL = process.env.DISCORD_AUTHORIZATION_URL;
-export const API_URL = process.env.API_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const ADMIN_ID = process.env.ADMIN_ID;
 export const BOT_AVATAR =
   "https://upload.wikimedia.org/wikipedia/en/e/e4/The_Guess_List.png";
