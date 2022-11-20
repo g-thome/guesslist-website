5f02953792f2efc8c41be00643fd78a24708d90d
Gabriel Thomé <gabrielallan.thome@gmail.com>
2022-11-20 09:25:44 -0300

    feat: add drafts page

--- src/Pages/my/drafts.tsx
+++ src/Pages/my/drafts.tsx
@@ -5,7 +5,6 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { createDraft, getUserLists } from "../../API";
 import { Page } from "../../components/Page";
-import { UserPlate } from "../../components/UserPlate";
 import { authOptions } from "../api/auth/[...nextauth]";
 import EditIcon from "@mui/icons-material/Edit";
 import BarChartIcon from "@mui/icons-material/BarChart";
@@ -23,41 +22,38 @@ export default function Drafts({ drafts }: Props) {
   }
 
   return (
-    <div className="p-8">
-      <UserPlate />
-      <Page title="Drafts">
-        {drafts.length ? (
-          <ul className="text-white text-xl">
-            {drafts.map((l, i) => (
-              <li key={"list" + i}>
-                {l.title || "Untitled"}{" "}
-                <EditIcon
-                  onClick={() => router.push(`/lists/${l.id}/edit`)}
-                  className="text-veryLightBlue cursor-pointer"
-                />{" "}
-                {l.status === ListStatus.PUBLISHED && (
-                  <BarChartIcon className="text-veryLightBlue cursor-pointer" />
-                )}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <div>
-            <p className="text-white">
-              You don&apos;t have any lists yet. Do you want to{" "}
-              <span
-                role="link"
-                onClick={handleCreateOneClick}
-                className="text-veryLightBlue cursor-pointer underline"
-              >
-                create one
-              </span>
-              ?
-            </p>
-          </div>
-        )}
-      </Page>
-    </div>
+    <Page title="Drafts">
+      {drafts.length ? (
+        <ul className="text-white text-xl">
+          {drafts.map((l, i) => (
+            <li key={"list" + i}>
+              {l.title || "Untitled"}{" "}
+              <EditIcon
+                onClick={() => router.push(`/lists/${l.id}/edit`)}
+                className="text-veryLightBlue cursor-pointer"
+              />{" "}
+              {l.status === ListStatus.PUBLISHED && (
+                <BarChartIcon className="text-veryLightBlue cursor-pointer" />
+              )}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>
+          <p className="text-white">
+            You don&apos;t have any lists yet. Do you want to{" "}
+            <span
+              role="link"
+              onClick={handleCreateOneClick}
+              className="text-veryLightBlue cursor-pointer underline"
+            >
+              create one
+            </span>
+            ?
+          </p>
+        </div>
+      )}
+    </Page>
   );
 }
 
