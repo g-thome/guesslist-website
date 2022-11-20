546743a7868b28d0266d086d5a3e70ccd233131c
Gabriel Thomé <gabrielallan.thome@gmail.com>
2022-11-20 09:25:25 -0300

    feat: add sidebar to Page component

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,9 +1,14 @@
+import Sidebar from "./Sidebar";
+
 export function Page({ children, title }) {
   return (
-    <div className="p8">
-      <div className="py-1 px-14">
-        <h1 className="text-white text-5xl mb-8 font-semibold">{title}</h1>
-        {children}
+    <div className="flex">
+      <Sidebar />
+      <div className="p8">
+        <div className="py-1 px-14">
+          <h1 className="text-white text-5xl mb-8 font-semibold">{title}</h1>
+          {children}
+        </div>
       </div>
     </div>
   );
