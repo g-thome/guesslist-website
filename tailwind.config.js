10dd310fd6d03788440714c4127236908779bd2e
Gabriel Thomé <gabrielallan.thome@gmail.com>
2022-11-27 03:41:13 -0300

    feat: use default tailwind colors in addition to the apps' colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,21 @@ module.exports = {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
     },
-    colors: {
-      onyx: "#36393f",
-      veryLightBlue: "#5966f3",
-      white: "#ffffff",
-      arsenic: "#40434c",
-      silverFoil: "#aeb0b3",
-      gray: "#b9bbbe",
-      darkCharcoal: "#303136",
-      black: "#000000",
-      green: "#369958",
-      spanishGray: '#96989e',
-      outerSpace: '#42464E'
-    },
+    extend: {
+      colors: {
+        onyx: "#36393f",
+        veryLightBlue: "#5966f3",
+        white: "#ffffff",
+        arsenic: "#40434c",
+        silverFoil: "#aeb0b3",
+        gray: "#b9bbbe",
+        darkCharcoal: "#303136",
+        black: "#000000",
+        green: "#369958",
+        spanishGray: '#96989e',
+        outerSpace: '#42464E'
+      }
+    }
   },
   plugins: [],
 };
