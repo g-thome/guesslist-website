bf32be5b0ab796738530769b25e17d794f4ae32d
Gabriel Thomé <gabrielallan.thome@gmail.com>
2022-11-27 03:28:54 -0300

    fix: wrong class name in page component

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,7 +4,7 @@ export function Page({ children, title }) {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="p8">
+      <div className="p-8">
         <div className="py-1 px-14">
           <h1 className="text-white text-5xl mb-8 font-semibold">{title}</h1>
           {children}
