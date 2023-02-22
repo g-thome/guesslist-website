c10544b7035b2163bb221ea753e692b69b9ca364
Gabriel Thomé <gabrielallan.thome@gmail.com>
2023-02-21 22:17:58 -0300

    fix: pages directory with capital P

---
+++ src/pages/api/list/[id].ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getListById, updateList } from "../../../db/list";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  return new Promise(async (resolve, reject) => {
+    if (!["GET", "PATCH"].includes(req.method)) {
+      res.status(405).end();
+      resolve();
+    }
+
+    if (req.method === "GET") {
+      try {
+        const { id } = req.query;
+        if (!id) {
+          res.status(400).json({ error: "Missing list id" });
+          resolve();
+        }
+        const list = await getListById(id as string);
+        res.status(200).json(list);
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    if (req.method === "PATCH") {
+      const { title, items, categories, language } = req.body;
+      const { id } = req.query;
+
+      if (!title && !items?.length && !categories?.length && !language) {
+        res
+          .status(400)
+          .json({ error: "Body must contain a valid update option" });
+        resolve();
+      }
+
+      try {
+        const result = await updateList(id as string, {
+          title,
+          items,
+          categories,
+          language,
+        });
+        res.status(200).json(result);
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    resolve();
+  });
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import "normalize.css";
+
+export default function Index() {
+  return <></>;
+}
