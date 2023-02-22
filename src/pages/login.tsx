5b37e6d7cbb0be0cfc7cb0c0edbf442864d279b5
Gabriel Thomé <gabrielallan.thome@gmail.com>
2023-02-22 01:30:11 -0300

    feat: adding pages directory

---
+++ src/pages/login.tsx
@@ -0,0 +1,24 @@
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+import React from "react";
+import DiscordLogo from "../../public/discord-logo.svg";
+
+export default function Login() {
+  return (
+    <div className="flex justify-center items-center flex-col h-screen">
+      <div className="mb-2">
+        <Image src={DiscordLogo} alt="discord" width={200} height={200} />
+      </div>
+      <button
+        onClick={() =>
+          signIn("discord", {
+            callbackUrl: "/my/drafts",
+          })
+        }
+        className="cursor-pointer bg-veryLightBlue outline-none border-0 py-2 px-4 text-white w-fit"
+      >
+        LOGIN WITH DISCORD
+      </button>
+    </div>
+  );
+}
