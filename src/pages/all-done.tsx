a572623321cdc1fd72459a73c198b0b82643addf
Gabriel Thomé <gabrielallan.thome@gmail.com>
2023-02-26 00:26:29 -0300

    fix: display sidebar on all done page

--- src/pages/all-done.tsx
+++ src/pages/all-done.tsx
@@ -2,28 +2,24 @@ import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { GetServerSidePropsContext } from "next/types";
 import { Page } from "../components/Page";
-import { UserPlate } from "../components/UserPlate";
 import { authOptions } from "src/pages/api/auth/[...nextauth]";
 
 
 export default function AllDone() {
   return (
-    <div className="p-8">
-      <UserPlate />
-      <Page title="">
-        <main>
-          <p className="text-white text-6xl">
-            All done!
-            <br />
-            Your list was submitted to review. Once it passes, the bot will send
-            you a DM if your Discord settings allow so. Meanwhile, you can also{" "}
-            <Link href="/my/drafts" className="text-veryLightBlue">
-                check the status of your lists.
-            </Link>
-          </p>
-        </main>
-      </Page>
-    </div>
+    <Page title="">
+      <main>
+        <p className="text-white text-4xl">
+          All done!
+          <br />
+          Your list was submitted to review. Once it passes, the bot will send
+          you a DM if your Discord settings allow so. Meanwhile, you can also{" "}
+          <Link href="/my/drafts" className="text-veryLightBlue">
+            check the status of your lists.
+          </Link>
+        </p>
+      </main>
+    </Page>
   );
 }
 
