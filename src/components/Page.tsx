9630c7e7f87405a90df7649dd1fb4a177d8ceb16
Gabriel Thomé <gabrielallan.thome@gmail.com>
2023-07-29 10:50:57 -0300

    fix: page width

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,7 +4,7 @@ export function Page({ children, title }) {
   return (
     <div className="flex h-screen w-screen">
       <Sidebar />
-      <div className="p-8 overflow-auto">
+      <div className="p-8 overflow-auto w-full">
         <div className="py-1 px-14">
           <h1 className="text-white text-5xl mb-8 font-semibold">{title}</h1>
           {children}
